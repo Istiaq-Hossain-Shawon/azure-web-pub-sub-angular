8c465d7e1ccb104e5261867a6611329053eceed6
Istiaq Hossain <hossain.ict@gmail.com>
2022-04-03 16:26:57 +0600

    add private method to get webpubsubUrl

--- src/app/common/webpubsub-constants.ts
+++ src/app/common/webpubsub-constants.ts
@@ -1,9 +1,9 @@
 export class WebPubSubConstants {
     public static connectionStringAzureWebPubSub: 
-    string = "Endpoint=https://webpubsubdemo.webpubsub.azure.com;AccessKey=+Xg8OsmnI8tf4iU6u5DqS8qOhdwZ5nU/bfbp4/3jIHo=;Version=1.0;";
+    string = "Endpoint=https://webpubsubdemo.webpubsub.azure.com;AccessKey=1bmoZnYDKSyzssf4NE1hGh1dD6qQL816F8NVFLjMbBo=;Version=1.0;";
       
-    public static clientAccessUrlAzureWebPubSub: 
-    string = "wss://webpubsubdemo.webpubsub.azure.com/client/hubs/Hub?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ3c3M6Ly93ZWJwdWJzdWJkZW1vLndlYnB1YnN1Yi5henVyZS5jb20vY2xpZW50L2h1YnMvSHViIiwiaWF0IjoxNjQ4OTYxMTM4LCJleHAiOjE2NDg5NjQ3Mzh9.Kz9rGdmJ9rVrp2y5ixfbOg6WVKxQr0hJMTRgQTQiIIo";
+    // public static clientAccessUrlAzureWebPubSub: 
+    // string = "wss://webpubsubdemo.webpubsub.azure.com/client/hubs/Hub?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ3c3M6Ly93ZWJwdWJzdWJkZW1vLndlYnB1YnN1Yi5henVyZS5jb20vY2xpZW50L2h1YnMvSHViIiwiaWF0IjoxNjQ4OTYxMTM4LCJleHAiOjE2NDg5NjQ3Mzh9.Kz9rGdmJ9rVrp2y5ixfbOg6WVKxQr0hJMTRgQTQiIIo";
 
     public static hubAzureWebPubSub: string = "Hub";
 
--- src/app/common/websocket/WebPubSub.ts
+++ src/app/common/websocket/WebPubSub.ts
@@ -6,27 +6,32 @@ import { WebPubSubConstants } from "src/app/common/webpubsub-constants";
 import { IWebSocket } from "./IWebSocket";
 
 @Injectable()
- export class WebPubSub implements IWebSocket {
-    getStatus(): Observable<any> {
-        
-        var observable = new Observable<any>((subcriber) => {
+export class WebPubSub implements IWebSocket {
+  getStatus(): Observable<any> {
 
-            var subject = webSocket(WebPubSubConstants.clientAccessUrlAzureWebPubSub)
-      
-            subject.subscribe(
-              msg => subcriber.next(msg),
-              err => console.log(err),
-              () => console.log('complete')
-            );
-          })
-      
-          return observable;
+    var observable = new Observable<any>((subcriber) => {
+      this.GetURl().then(value => {
+        var subject = webSocket(value)
+        subject.subscribe(
+          msg => subcriber.next(msg),
+          err => console.log(err),
+          () => console.log('complete')
+        );
+      })
+    })
+
+    return observable;
 
 
-    }
-    async publishStatus(information: any) {
-        const serviceClient = new WebPubSubServiceClient(WebPubSubConstants.connectionStringAzureWebPubSub, WebPubSubConstants.hubAzureWebPubSub);
-        await serviceClient.sendToAll(information);
-    }
+  }
+  async publishStatus(information: any) {
+    const serviceClient = new WebPubSubServiceClient(WebPubSubConstants.connectionStringAzureWebPubSub, WebPubSubConstants.hubAzureWebPubSub);
+    await serviceClient.sendToAll(information);
+  }
+  private async GetURl(): Promise<string> {
+    const service = new WebPubSubServiceClient(WebPubSubConstants.connectionStringAzureWebPubSub, WebPubSubConstants.hubAzureWebPubSub);
+    let token = await service.getClientAccessToken();
+    return token.url;
+  }
 
 }
